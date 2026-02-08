a858a8d1b70ee525bd85cf9a69462dfc13f79af2
ImaneJb1 <imaane49@gmail.com>
2026-02-08 22:43:11 +0100

    salit

--- ex02/Array.hpp
+++ ex02/Array.hpp
@@ -1,14 +1,53 @@
 #pragma once
-
+#include "exception"
+#include "iostream"
 template <typename T>
 class Array
 {
     private:
         T*            data;
-        unsigned int  size;
+        unsigned int  _size;
     public:
-        Array() : data(NULL), size(0){}
-        Array(unsigned int n) : size(n), {
-            T Array[n]();// this is not understood
+        Array() : data(NULL), _size(0){}
+        Array(unsigned int n) : _size(n) {
+            data = new T[n](); //alloc
+        }
+
+        ~Array(){
+            delete[] data; //free
+        }
+        Array &operator=(const Array &other){
+            if(this == &other)
+                return *this;
+            delete[] data;
+            _size = other._size;
+            data = new T[_size]; //alloc
+            for(unsigned int i = 0; i < _size; i++)
+                data[i] = other.data[i];
+            return *this;
+        }
+
+        Array(const Array &other){
+            _size = other._size;
+            data = new T[_size]; //alloc
+            for(unsigned int i = 0; i < _size; i++)
+                data[i] = other.data[i];
+        }
+        
+        unsigned int size(void) const{
+            return _size;
+        }
+
+        T &operator[](unsigned int index){
+            if(index >= _size)
+                throw std::exception();
+            return data[index];
+        }
+
+        const T &operator[](unsigned int index) const{
+            if(index >= _size)
+                throw std::exception();
+            return data[index];
         }
 };
+
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -3,7 +3,40 @@
 
 int main()
 {
-    int *a = new int();
-    std::cout << *a << "\n";
+
+    {
+    Array<int> obj(10);
+    Array<int> b(obj);
+    std::cout << "b size = "<< b.size() << std::endl;
+    try
+    {
+        std::cout << "obj[9] = " << obj[9] << "\n\n" ;
+        obj[9] = 25;
+        for(unsigned int i = 0; i < obj.size(); i++)
+            std::cout << "obj[" << i << "] = " << obj[i] << "\n";
+        b = obj;
+        std::cout << "b[9] = " << b[9] << "\n\n" ;
+
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    }
+    /////////////////////////////////////////////////
+    Array<std::string> strings(5);
+    try
+    {
+        for(unsigned int i = 0; i < strings.size(); i++)
+            strings[i] = "imane";
+        Array<std::string> const a(strings);
+        for(unsigned int i = 0; i < a.size(); i++)
+            std::cout << a[i] << "\n";
+    }
+    catch(const std::exception &e)
+    {
+        std::cout << e.what() << std::endl;
+    }
+
     return 0;
 }
